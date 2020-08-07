24a10853134ecee57c636eab8edab2da55501409
painor <30215770+painor@users.noreply.github.com>
2020-08-07 03:31:37 +0100

    Add websocket chat starter

--- watch/urls.py
+++ watch/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home,name="watch-home"),
+    path('', views.home, name="watch-home"),
+    path('<str:room_name>/', views.room, name='room'),
 ]
